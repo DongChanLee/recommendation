f622867bf5c8408177d722892b8a32d63c4fc418
dc <evanlee0407@gmail.com>
2022-10-26 18:01:02 +0900

    성동구 카페정보 추가 수집

--- cafe/crawl.py
+++ cafe/crawl.py
@@ -14,39 +14,66 @@ import time
 
 # 'https://dapi.kakao.com/v2/local/search/keyword.json?page=1&size=15&sort=accuracy&query=%EA%B0%95%EB%82%A8%EA%B5%AC+%EC%B9%B4%ED%8E%98&category_group_code=CE7'
 
-#areas = ['종로구']
-areas = ['종로구', '중구', '용산구', '성동구', '광진구', 
-         '동대문구', '중랑구', '성북구', '강북구', '도봉구', 
-         '노원구', '은평구', '서대문구', '마포구', '양천구', 
-         '강서구', '구로구', '금천구', '영등포구', '동작구', 
-         '관악구', '서초구', '강남구', '송파구', '강동구']
+areas = ['종로구', '성동구']
+# areas = ['종로구', '중구', '용산구', '성동구', '광진구', 
+#          '동대문구', '중랑구', '성북구', '강북구', '도봉구', 
+#          '노원구', '은평구', '서대문구', '마포구', '양천구', 
+#          '강서구', '구로구', '금천구', '영등포구', '동작구', 
+#          '관악구', '서초구', '강남구', '송파구', '강동구']
 
 url = 'https://dapi.kakao.com/v2/local/search/keyword.json'
 
 def get_cafe_info():
-    data = []
     for area in areas:
-        for i in range(1, 3):  # 최대 page=45
+        data = []
+        for i in range(1, 46):  # 최대 page=45
             params = {'page': i, 'size': 15, 'query': area, 'category_group_code': 'CE7'}
             res = requests.get(url=url, params=params, headers=HEADERS, verify=False)
             res.encoding = 'utf-8'
-            res = res.json()       
-            for j in res['documents']:
-                new_data = {
-                    'id': j['id'], 'place_name': j['place_name'], 'address_name': j['address_name'], 
-                    'road_address_name': j['road_address_name'], 'phone': j['phone'], 
-                    'category_group_name': j['category_group_name'], 'category_name': j['category_name'], 'place_url': j['place_url'] 
-                }
-                data.append(new_data)            
+            res = res.json()
+            if res['meta']['is_end'] == 'true':    
+                break
+            else:
+                for j in res['documents']:
+                    new_data = {
+                        'id': j['id'], 'place_name': j['place_name'], 'address_name': j['address_name'], 
+                        'road_address_name': j['road_address_name'], 'phone': j['phone'], 
+                        'category_group_name': j['category_group_name'], 'category_name': j['category_name'], 'place_url': j['place_url'] 
+                    }
+                    data.append(new_data)
+                time.sleep(1.0)            
         file_name = area + '_' + '카페 정보' + '.json'
         with open(os.path.join(CAFE['CRAWL_DATA_PATH'], file_name), 'w', encoding='utf-8') as file:
             file.write(json.dumps(data, indent=4, ensure_ascii=False))
-        time.sleep(0.5)
+        time.sleep(3.0)
+
+# def get_cafe_info():
+#     data = []
+#     for area in areas:
+#         for i in range(1, 46):  # 최대 page=45
+#             params = {'page': i, 'size': 15, 'query': area, 'category_group_code': 'CE7'}
+#             res = requests.get(url=url, params=params, headers=HEADERS, verify=False)
+#             res.encoding = 'utf-8'
+#             res = res.json()       
+#             for j in res['documents']:
+#                 new_data = {
+#                     'id': j['id'], 'place_name': j['place_name'], 'address_name': j['address_name'], 
+#                     'road_address_name': j['road_address_name'], 'phone': j['phone'], 
+#                     'category_group_name': j['category_group_name'], 'category_name': j['category_name'], 'place_url': j['place_url'] 
+#                 }
+#                 data.append(new_data)
+#             time.sleep(0.5)            
+#         file_name = area + '_' + '카페 정보' + '.json'
+#         with open(os.path.join(CAFE['CRAWL_DATA_PATH'], file_name), 'w', encoding='utf-8') as file:
+#             file.write(json.dumps(data, indent=4, ensure_ascii=False))
+#         time.sleep(0.5)
 
 
 def json_to_csv():
-    df = pd.read_json(os.path.join(CAFE['CRAWL_DATA_PATH'], '종로구_카페 정보.json'))
-    df.to_csv(os.path.join(CAFE['CSV_DATA_PATH'], '종로구_카페 정보.csv'))
+    for area in areas:
+        file_name = area + '_' + '카페 정보' + '.json'
+        df = pd.read_json(os.path.join(CAFE['CRAWL_DATA_PATH'], file_name))
+        df.to_csv(os.path.join(CAFE['CSV_DATA_PATH'], area + '_카페 정보.csv'))
 
 
 def json_to_rating():
@@ -142,7 +169,7 @@ def get_cafe_info_test():
 
 if __name__ == "__main__": 
     # get_cafe_info_test()
-    # get_cafe_info()
-    # json_to_csv()
+    get_cafe_info()
+    json_to_csv()
     # get_blog_review()
-    json_to_rating()
+    # json_to_rating()
