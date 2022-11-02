70aa2c483fbec1a4fbe50634cb276ef5a385dc16
dc <evanlee0407@gmail.com>
2022-11-02 17:56:16 +0900

    cafe Original 모델 수정

--- cafe/models.py
+++ cafe/models.py
@@ -1,20 +1,34 @@
-from email import message
 from django.db import models
 from django.urls import reverse
+from django.core.validators import MinValueValidator, MaxValueValidator
 
 class Original(models.Model):
     '''
     카페 정보 원본 테이블
     '''
-    id = models.IntegerField(primary_key=True)
+    # id = models.IntegerField(primary_key=True)
+    # place_area = models.CharField(max_length=10)
+    # place_name = models.TextField(verbose_name='상호명')
+    # address_name = models.TextField(verbose_name='지번 주소')
+    # road_address_name = models.TextField(verbose_name='도로명 주소')
+    # phone = models.CharField(max_length=20, verbose_name='전화번호')
+    # category_group_name = models.CharField(max_length=20)
+    # category_name = models.TextField(blank=True)
+    # place_url = models.URLField(max_length=200, verbose_name='위치정보 URL')
+    # is_public = models.BooleanField(default=False, verbose_name='공개여부')
+    # create_dt = models.DateTimeField(auto_now_add=True)
+    # update_dt = models.DateTimeField(auto_now=True, verbose_name='최종 수정 날짜')
+
+    id = models.AutoField(primary_key=True)
     place_area = models.CharField(max_length=10)
     place_name = models.TextField(verbose_name='상호명')
     address_name = models.TextField(verbose_name='지번 주소')
     road_address_name = models.TextField(verbose_name='도로명 주소')
-    phone = models.CharField(max_length=20, verbose_name='전화번호')
+    phone = models.CharField(null=True, max_length=20, verbose_name='전화번호')
     category_group_name = models.CharField(max_length=20)
-    category_name = models.TextField(blank=True)
+    category_name = models.TextField(null=True)
     place_url = models.URLField(max_length=200, verbose_name='위치정보 URL')
+    stars = models.FloatField(default=3.00, validators=[MinValueValidator(0.1),MaxValueValidator(5.0)])
     is_public = models.BooleanField(default=False, verbose_name='공개여부')
     create_dt = models.DateTimeField(auto_now_add=True)
     update_dt = models.DateTimeField(auto_now=True, verbose_name='최종 수정 날짜')
--- common/insert_data.py
+++ common/insert_data.py
@@ -13,34 +13,70 @@ from datetime import datetime
 import psycopg2
 from common.conn_db import connect_db
 import json
+from cafe.models import Original
 
 
 class InsertData():
+    # def insert_cafe(self):
+    #     from cafe.models import Original
+    #     for area in CAFE['AREAS']:
+    #         file_name =  area + '_' + '카페 정보' + '.json'
+    #         try:
+    #             with open(os.path.join(CAFE['CRAWL_DATA_PATH'], file_name), 'r', encoding='utf-8') as file:
+    #                 json_data = json.load(file)
+    #                 # print(json_data)
+    #                 for data in json_data:
+    #                     id = data['id']
+    #                     place_name = data['place_name']
+    #                     address_name = data['address_name']
+    #                     road_address_name = data['road_address_name']
+    #                     phone = data['phone']
+    #                     category_group_name = data['category_group_name']
+    #                     category_name = data['category_name']
+    #                     place_url = data['place_url']
+
+    #                     Original.objects.create(
+    #                         id=id, place_area=area, place_name=place_name, address_name=address_name, road_address_name=road_address_name,
+    #                         phone=phone, category_group_name=category_group_name, category_name=category_name, place_url=place_url
+    #                     )
+    #         except FileNotFoundError:
+    #             pass
+
     def insert_cafe(self):
-        from cafe.models import Original
-        for area in CAFE['AREAS']:
-            file_name =  area + '_' + '카페 정보' + '.json'
-            try:
-                with open(os.path.join(CAFE['CRAWL_DATA_PATH'], file_name), 'r', encoding='utf-8') as file:
-                    json_data = json.load(file)
-                    # print(json_data)
-                    for data in json_data:
-                        id = data['id']
-                        place_name = data['place_name']
-                        address_name = data['address_name']
-                        road_address_name = data['road_address_name']
-                        phone = data['phone']
-                        category_group_name = data['category_group_name']
-                        category_name = data['category_name']
-                        place_url = data['place_url']
+        file_name1 =  '서울특별시 카페 정보_url.json'
+        file_name2 =  '서울특별시 카페 review.json'
+        try:
+            with open(os.path.join(CAFE['CRAWL_DATA_PATH'], file_name1), 'r', encoding='utf-8') as file1:
+                json_data1 = json.load(file1)
+                print('file1 읽어오기 성공')
+        except FileNotFoundError as e:
+            print(f'file1 미존재: {e}')
+        try:
+            with open(os.path.join(CAFE['CRAWL_DATA_PATH'], file_name2), 'r', encoding='utf-8') as file2:
+                json_data2 = json.load(file2)
+                print('file2 읽어오기 성공')
+        except FileNotFoundError as e:
+            print(f'file2 미존재: {e}')
 
-                        Original.objects.create(
-                            id=id, place_area=area, place_name=place_name, address_name=address_name, road_address_name=road_address_name,
-                            phone=phone, category_group_name=category_group_name, category_name=category_name, place_url=place_url
-                        )
-            except FileNotFoundError:
-                pass
+        for data in json_data1:
+            place_name = data['place_name']
+            address_name = data['address_name']
+            road_address_name = data['road_address_name']
+            phone = data['phone']
+            category_group_name = data['category_group_name']
+            place_url = data['naver_map_url']
 
+            print(place_name)
+
+        for data in json_data2:
+            if data['stars'] != 'null':
+                stars = data['stars']
+                print(stars)
+
+        Original.objects.create(
+            place_name=place_name, address_name=address_name, road_address_name=road_address_name,
+            phone=phone, category_group_name=category_group_name, place_url=place_url, stars=stars
+        )
 
 # def test_func(args=()):
 #     print(args)
